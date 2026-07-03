9cfd0c50bb90268c8681e81eab0e11b2caa85aa1
Vijayasri D <vijayasri.d05@gmail.com>
2026-07-03 15:03:03 +0530

    Configure vercel deployment and update wsgi entry point

--- SOURCE CODE/Voting/wsgi.py
+++ SOURCE CODE/Voting/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Voting.settings')
 
 application = get_wsgi_application()
+
+# Add this line below to let Vercel bind to the application instance smoothly
+app = application
